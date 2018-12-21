cf185c01683e64ecb28b67d3c4202afbba3acb9e
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2018-12-20 20:57:53 -0600

    Adding dummy hardware maps and opmodes
    This is to fix telemetry

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/noHWMap.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class noHWMap {
+    // This hardware map is for testing and should not at all be used in competition
+    // as this is hardware map has literally nothing in it at all.
+
+    // Declaring and Initializing hardware map
+    HardwareMap noHWMap = null;
+    private ElapsedTime timePeriod = new ElapsedTime();
+
+    // Constructor class
+    // This is used to create an object that can be used by other classes and can take in input
+    // from other classes, for instance such as our OpMode
+    public noHWMap() {
+
+    }
+
+    // Initialize standard Hardware interfaces
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        noHWMap = ahwMap;
+    }
+}
