8f07230598c1c844ee2dc3e0c815bd38024a8b4f
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2019-02-08 22:01:31 -0600

    Added arm movements for autonomous
    This is untested and should be tested more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousShortOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousShortOpMode.java
@@ -57,6 +57,8 @@ public class AutonomousShortOpMode extends LinearOpMode {
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
+        // S0: Move lift down
+        encoderArm(robot.ARM_DOWN_LOW_POWER_VAL, 2, 5.0);
         // S1: Forward 47 Inches with 5 Sec timeout
         encoderDrive(driveSpeed,  48,  48, 5.0);
         // S2: Turn Right 12 Inches with 4 Sec timeout
@@ -109,6 +111,51 @@ public class AutonomousShortOpMode extends LinearOpMode {
             // Turn off RUN_TO_POSITION
             robot.leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             robot.rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+    }
+
+    public void encoderArm(double speed, double liftInches, double timeoutS) {
+        int newLiftTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLiftTarget = robot.leftFrontDrive.getCurrentPosition() + (int)(liftInches * armCountsPerInch);
+            robot.armMotor1.setTargetPosition(newLiftTarget);
+            robot.armMotor2.setTargetPosition(newLiftTarget);
+
+            // Turn On RUN_TO_POSITION
+            robot.armMotor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.armMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            opModeRuntime.reset();
+            robot.armMotor1.setPower(Math.abs(speed));
+            robot.armMotor2.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (opModeIsActive() &&
+                    (opModeRuntime.seconds() < timeoutS) && (robot.leftFrontDrive.isBusy() && robot.rightFrontDrive.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to %7d :%7d", newLiftTarget);
+                telemetry.addData("Path2",  "Running at %7d :%7d", robot.armMotor1.getCurrentPosition(), robot.armMotor2.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            robot.armMotor1.setPower(0);
+            robot.armMotor2.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            robot.armMotor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.armMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
     }
 }
